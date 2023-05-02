864c6bbcf8e153caa310e2e50182376bc4074c75
Manu Srivastava <92544857+ManuSrivastava1@users.noreply.github.com>
2023-05-02 17:20:38 +0200

    created impl.py

---
+++ impl.py
@@ -0,0 +1,42 @@
+import ModelClasses as mdc
+import pandas as pd
+
+# 1) Importing all the classes for handling the relational database
+from relationalData_Manager import RelationalDataProcessor, RelationalQueryProcessor
+
+# 2) Importing all the classes for handling RDF database
+from graphData_Manager import TriplestoreDataProcessor, TriplestoreQueryProcessor
+
+# 3) Importing the class for dealing with generic queries
+from GenericQueryP import GenericQueryProcessor
+
+# Once all the classes are imported, first create the relational
+# database using the related source data
+rel_path = "relational.db"
+rel_dp = RelationalDataProcessor()
+rel_dp.setDbPath(rel_path)
+rel_dp.uploadData("data/relational_publications.csv")
+rel_dp.uploadData("data/relational_other_data.json")
+
+# Then, create the RDF triplestore (remember first to run the
+# Blazegraph instance) using the related source data
+grp_endpoint = "http://127.0.0.1:9999/blazegraph/sparql"
+grp_dp = TriplestoreDataProcessor()
+grp_dp.setEndpointUrl(grp_endpoint)
+grp_dp.uploadData("data/graph_publications.csv")
+grp_dp.uploadData("data/graph_other_data.json")
+
+# In the next passage, create the query processors for both
+# the databases, using the related classes
+rel_qp = RelationalQueryProcessor()
+rel_qp.setDbPath(rel_path)
+
+grp_qp = TriplestoreQueryProcessor()
+grp_qp.setEndpointUrl(grp_endpoint)
+
+# Finally, create a generic query processor for asking
+# about data
+generic = GenericQueryProcessor()
+generic.addQueryProcessor(rel_qp)
+generic.addQueryProcessor(grp_qp)
+
